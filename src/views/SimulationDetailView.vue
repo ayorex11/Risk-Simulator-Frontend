b09f477c398a1134ba43c354b49edfc208b4803b
Ayorex11 <adewalemaxwell11@gmail.com>
2026-04-29 14:02:44 +0100

    move to prod

--- src/views/SimulationDetailView.vue
+++ src/views/SimulationDetailView.vue
@@ -638,7 +638,7 @@ const handleWhatIfComplete = (newSimulation) => {
 const downloadPDF = async () => {
   try {
     const simulationId = route.params.id
-    const response = await api.get(`/simulations/${simulationId}/report/`, { responseType: 'blob' })
+    const response = await api.get(`/simulations/${simulationId}/report/pdf/`, { responseType: 'blob' })
     const url = window.URL.createObjectURL(new Blob([response.data]))
     const link = document.createElement('a')
     link.href = url
